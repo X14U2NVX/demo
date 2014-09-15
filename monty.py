8255429510947266554d9829d08996db9c416c02
X14U2NVX <X14U2NVX@gmail.com>
2014-09-15 18:16:48 -0400

    THIS IS THE FIRST VERSION

---
+++ monty.py
@@ -0,0 +1,9 @@
+answer = raw_input('What is your name? ')
+if answer != 'Lancelot':
+	exit()
+answer = raw_input('What is your quest? ')
+if answer != 'Holy Grail':
+	exit()
+answer = raw_input('What.... is your favorite color? ')
+if answer != 'Blue':
+	exit()
